fa18dd86b6c1bb37b08c70bbec94607dbcc22f9b
Enes Birol <enesbirool@gmail.com>
2025-02-08 20:11:34 +0300

    soap entegration

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const registerJobseeker = require("../controller/auth/registerJobseeker");
 const registerEmployer = require("../controller/auth/registerEmployer");
 const login = require("../controller/auth/login");
 const { validateRegisterEmployer, validateRegisterJobseeker } = require("../middlewares/validationMiddleware");
-const checkTempEmail = require("../core/validations/checkTempEmail");
+const checkTempEmail = require("../core/validations/auth/checkTempEmail");
 
 router.post("/register/jobseeker", validateRegisterJobseeker, checkTempEmail, registerJobseeker);
 router.post("/register/employer", validateRegisterEmployer, checkTempEmail, registerEmployer);
